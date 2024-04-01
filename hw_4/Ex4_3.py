3b3013e20d8d794cb96dc8385306b5a2c2f5b11d
Derkach Denis <144727627+Dpbt@users.noreply.github.com>
2024-04-02 01:29:22 +0300

    Update Ex4_3.py

--- hw_4/Ex4_3.py
+++ hw_4/Ex4_3.py
@@ -1,28 +1,52 @@
-import sys
 import time
 import multiprocessing
+from threading import Thread
 import codecs
 
 
-def process_main(conn_out, conn_in, messages_in, start, queue):
-    for message_in in messages_in:
-        conn_out.send(message_in)
-    while conn_in.poll(timeout=7):
-        message_recv = conn_in.recv()
-        queue.put([time.time() - start, message_recv])
+def process_main(conn_out, conn_in, start, queue):
+    def sending():
+        stdin = open(0)
+        for line in stdin:
+            conn_out.send(line)
+            queue.put(f"Time from start:{time.time() - start}: process_main sent \"{line[:-2]}\"\n")
+
+    def printing():
+        while conn_in.poll(timeout=60):
+            message_recv = conn_in.recv()
+            queue.put(f"Time from start:{time.time() - start}: process_main get \"{message_recv[:-2]}\"\n")
+
+    therad_sending = Thread(target=sending, args=())
+    therad_printing = Thread(target=printing, args=())
+    therad_sending.start()
+    therad_printing.start()
+    therad_sending.join()
+    therad_printing.join()
 
 
-def process_a(conn_out, conn_in, queue):
-    while conn_in.poll(timeout=0.05):
-        new_massage = conn_in.recv()
-        queue.put(new_massage)
-    while not queue.empty():
-        conn_out.send(queue.get().lower())
-        time.sleep(5)
+def process_a(conn_out, conn_in, start, queue):
+    def getting():
+        while conn_in.poll(timeout=60):
+            new_message = conn_in.recv()
+            queue.put(new_message)
+
+    def sending():
+        while not queue.empty():
+            message_out = queue.get()
+            conn_out.send(message_out)
+            time.sleep(5)
+
+    therad_getting = Thread(target=getting, args=())
+    therad_getting.start()
+    while therad_getting.is_alive():
+        therad_sending = Thread(target=sending, args=())
+        therad_sending.start()
+        therad_sending.join()
+    therad_getting.join()
 
 
-def process_b(conn_out, conn_in):
-    while conn_in.poll(timeout=6):
+def process_b(conn_out, conn_in, start):
+    while conn_in.poll(timeout=60):
         new_massage = conn_in.recv()
         new_massage = codecs.encode(new_massage, 'rot_13')
         conn_out.send(new_massage)
@@ -30,12 +54,6 @@ def process_b(conn_out, conn_in):
 
 if __name__ == '__main__':
     file = open("Ex4_3_processes.txt", "w")
-
-    messages = [line for line in sys.stdin.readlines()]
-
-    file.write("Input from keyboard:\n")
-    for message in messages:
-        file.write(message)
 
     a_conn1, main_conn1 = multiprocessing.Pipe()
     b_conn1, a_conn2 = multiprocessing.Pipe()
@@ -46,9 +64,9 @@ if __name__ == '__main__':
 
     start_time = time.time()
     main = multiprocessing.Process(target=process_main,
-                                   args=(main_conn1, main_conn2, messages, start_time, queue_main,))
-    pr_a = multiprocessing.Process(target=process_a, args=(a_conn2, a_conn1, queue_a,))
-    pr_b = multiprocessing.Process(target=process_b, args=(b_conn2, b_conn1,))
+                                   args=(main_conn1, main_conn2, start_time, queue_main,))
+    pr_a = multiprocessing.Process(target=process_a, args=(a_conn2, a_conn1, start_time, queue_a,))
+    pr_b = multiprocessing.Process(target=process_b, args=(b_conn2, b_conn1, start_time,))
 
     main.start()
     pr_a.start()
@@ -58,8 +76,6 @@ if __name__ == '__main__':
     pr_a.join()
     pr_b.join()
 
-    file.write("Output from main process:\n")
-
     while not queue_main.empty():
         message = queue_main.get()
-        file.write(f"Message:\"{message[1].rstrip("\n")}\", Time from start: {message[0]}, \n")
+        file.write(message)
